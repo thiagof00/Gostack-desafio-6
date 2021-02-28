de7636c5497c142702a244fc79f19cdc64fb5fff
thiagof00 <thiagoribeiro0302@gmail.com>
2021-02-28 19:20:53 -0300

    primeira att

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,18 +1,25 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+ import TransactionsRepository from '../repositories/TransactionsRepository';
+ import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+
+
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+
+  const {title, value, type, category} = request.body
+  const transactionService = new CreateTransactionService()
+
+  const transaction = await transactionService.execute({title, value, type, category})
+
+  response.json(transaction)
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,30 @@
 // import AppError from '../errors/AppError';
-
+import {getCustomRepository} from 'typeorm'
+import TransactionRepository from '../repositories/TransactionsRepository'
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
+  public async execute({title, value, type, category}:Request): Promise<Transaction> {
     // TODO
+    const transactionRepository = getCustomRepository(TransactionRepository)
+    // const categoryRepository = getRepository(Category)
+
+    const newTransaction = transactionRepository.create({
+      title,
+      value,
+      type
+    })
+    await transactionRepository.save(newTransaction)
+
+    return newTransaction
   }
 }
 
